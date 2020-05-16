73626fef849eb6c2a29de7daa486771a8927e2b4
julietvt <freshitpt@gmail.com>
2020-05-16 14:36:30 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -7,29 +7,38 @@ const date = new Date();
 const DELAY = 10;
 let intervalID = null;
 timerStartElem.onclick = function () {
-    if(intervalID){ return; }
+    if(intervalID){
+        return;
+    }
     intervalID = setInterval(incSecond, DELAY);
 };
+
 timerStopElem.onclick = function (){
     if(intervalID){
         clearInterval(intervalID);
         intervalID = null;
-    }};
+    }
+};
+
 timerResetElem.onclick = function(){
     //date.setHours(0,0,0,0);
     date.setMinutes(0,0,0);
     updateTimerValue();
 }
+
 function incSecond() {
     date.setMilliseconds(date.getMilliseconds() + DELAY);
     updateTimerValue();
 }
+
 function updateTimerValue(){
-    timerValueElem.innerText = `${format2(date.setMinutes())}:${format2(date.getSeconds())}:${format3(date.getMilliseconds())}`;
+    timerValueElem.innerText = `${format2(date.getMinutes())}:${format2(date.getSeconds())}:${format3(date.getMilliseconds())}`;
 }
+
 function format2(value) {
-    return value < 10 ? `0${value}`: `${value}`;
+    return value < 10 ? `0${value}`: value;
 }
-function format3(value) {
-    return value < 100 ? value < 10 ? `00${value}`: `$0{value}` : value`;
+
+function format3(value){
+    return value < 100 ? value < 10 ? `00${value}`: `0${value}` : value;
 }
